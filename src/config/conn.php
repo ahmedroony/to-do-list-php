db9fe3dc2e9420b0ddf6bde45798629dd81af89a
ahmed sayed <roonya973@gmail.com>
2025-09-25 20:48:42 +0300

    ana abdnt

--- src/config/conn.php
+++ src/config/conn.php
@@ -1,31 +1,14 @@
 <?php
 namespace ToDoList\Ahmed\config;
 class conn {
-       public $servername;
-       public $username;
-       public $password;
-   public function __construct($servername,$username,$password)
-   {
-        $this->servername  = $servername;
-        $this->username = $username;
-        $this->password = $password;
-   }
-   public function connect(){
-    $conn = new mysqli($this->servername , $this->username , $this->password);
-    if($this->$conn->connect_error){
-        die("connection failed" . $conn->connect_error);
+    public $servername = "localhost";
+    public $username = "root";
+    public $password = "";
+    public $dbname = "todolist";
+    public function conn(){
+        $conn = new \mysqli($this->servername,$this->username,$this->password,$this->dbname);
+        if($conn->connect_error){
+            ("pls check") . $conn->connect_error;
+        }
     }
-    echo "connected successfully";
-    //create DB
-    $sql = "CREATE DATABASE IF NOT EXISTS todolist2";
-    if($conn->query($sql) === TRUE){    
-        echo "db created";
-    }else{
-        echo "failed to create db";
-    }
-    $conn->close();
-   }
 }
-$connOne = new conn("localhost","ahmed22","123456789");
-$connOne->connect();
-?>
--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,6 @@
-<?php
-require_once('vendor/autoload.php');
+<!-- <?php
+    require("vendor/autoload.php");
     use ToDoList\Ahmed\conn;
-    use ToDoList\Ahmed\create;
-    require_once("src/UI/main.php");
-?>
+    $userOne = new conn();
+    // require_once("src/UI/main.php");
+?> -->
