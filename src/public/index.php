85ff41f1c29d3bf5bad6f2f66c8af86695a6a3cd
ahmed sayed <roonya973@gmail.com>
2025-10-08 20:44:40 +0300

    remove some lines

--- src/public/index.php
+++ src/public/index.php
@@ -1,8 +1,5 @@
 <?php
 use ToDoList\Ahmed\Core\Database\DB;
 use ToDoList\Ahmed\Config\DbConfig;
-include "src/Core/Helper/basic.php";
-require __DIR__ . "/../../vendor/autoload.php";
 $DbOne = new DB;
-$DbOne->query('SELECT * FROM todolist');
 ?>
