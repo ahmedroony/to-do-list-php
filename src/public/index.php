95ccf65e0d0766d3cc1a611ec3211ae1255192df
ahmed sayed <roonya973@gmail.com>
2025-10-28 22:50:25 +0300

    try to understand confilect in github

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,22 @@
 <?php
 use ToDoList\Ahmed\Core\Database\DB;
 use ToDoList\Ahmed\Config\DbConfig;
-$DbOne = new DB;
+require __DIR__ . '/../../vendor/autoload.php';
+$uri = $_SERVER['SCRIPT_NAME'];
+
+$route = ['/', 'create'];
+
+function routes($route,$uri)
+{
+    foreach ($route as $routes) {
+        if ($routes == $uri) {
+            echo $uri . ':allowd';
+            return true;
+            die();
+        }
+        return false;
+    }
+}
+echo routes($route,$uri);
+
 ?>
