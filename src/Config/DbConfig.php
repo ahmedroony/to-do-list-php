652470f1158c5392a2b30f6c03332fc0c979ea7b
Khaled Fathi <dev@khaledfathi.com>
2025-10-31 00:15:22 +0200

    Revert "merge dev (#1)"
    This reverts commit c430e2fc3cc8f8024c019673a85a080f383f98e9.

--- src/Config/DbConfig.php
+++ src/Config/DbConfig.php
@@ -20,7 +20,10 @@ class DbConfig
         //read the file as string
         $readConfigFile = file_get_contents(Constant::CONFIG_File_Path());// if the path change what should we do 
         //return the file as associative arrays
-        return $arrayConfig = json_decode($readConfigFile); 
+        if ($readConfigFile) {
+            $arrayConfig = json_decode($readConfigFile);
+        }
+        return $arrayConfig;
     }
     public function showConfig(){
         return[
--- src/public/index.php
+++ src/public/index.php
@@ -1,22 +1,5 @@
 <?php
 use ToDoList\Ahmed\Core\Database\DB;
 use ToDoList\Ahmed\Config\DbConfig;
-require __DIR__ . '/../../vendor/autoload.php';
-$uri = $_SERVER['SCRIPT_NAME'];
-
-$route = ['/', 'create'];
-
-function routes($route,$uri)
-{
-    foreach ($route as $routes) {
-        if ($routes == $uri) {
-            echo $uri . ':allowd';
-            return true;
-            die();
-        }
-        return false;
-    }
-}
-echo routes($route,$uri);
-
+$DbOne = new DB;
 ?>
